76b38d6c322194286e312288fb27a81140d0be31
Thaonnor <thaonnor@gmail.com>
2025-07-24 21:17:05 -0400

    Initial Vue + Vite setup

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+    <div id="app">
+        <h1>Welcome to Tally!</h1>
+        <p>Hello, world!</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'App',
+    };
+</script>
